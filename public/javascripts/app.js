0c448abda0a53b17ce5120a5833f7dfff3786cdb
sirivan <vanyaturianitsa@gmail.com>
2015-09-21 15:36:48 +0300

    starting pos

---
+++ public/javascripts/app.js
@@ -0,0 +1,35 @@
+var app = angular.module('app', []);
+app.factory('Socket', function($rootScope) {
+    var socket = io.connect();
+    return {
+        on: function(eventName, callback) {
+            socket.on(eventName, function() {
+                var args = arguments;
+                $rootScope.$apply(function() {
+                    callback.apply(socket, args);
+                });
+            });
+        },
+        emit: function(eventName, data, callback) {
+            socket.emit(eventName, data, function() {
+                var args = arguments;
+                $rootScope.$apply(function() {
+                    if (callback) {
+                        callback.apply(socket, args);
+                    }
+                });
+            })
+        }
+    };
+});
+
+app.controller('main', ['$scope', 'Socket',
+    function($scope, Socket) {
+        $scope.avalible = false;
+        $scope.message = '';
+
+        Socket.on('avalible', function(data) {
+            $scope.avalible = data;
+        });
+    }
+]);
